1f6a7aef099b2a8d7487d8dea2471fe4981defca
Dmitriy Pospelov <dmitriypospelov93@gmail.com>
2026-05-23 20:44:47 +0700

    Configure backend CORS for production

--- backend/src/main/java/com/example/taskmanager/task/TaskController.java
+++ backend/src/main/java/com/example/taskmanager/task/TaskController.java
@@ -7,7 +7,6 @@ import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "http://localhost:3000")
 public class TaskController {
 
     private final TaskService taskService;
